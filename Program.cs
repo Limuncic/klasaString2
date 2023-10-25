070e38a21f3723d60eadd3f2a47eba0f90cbeb39
Limuncic <145533801+Limuncic@users.noreply.github.com>
2023-10-25 08:33:04 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kalsaString2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Unesi rečenicu: ");
+            string s1= Console.ReadLine();
+
+            Console.WriteLine(s1.Replace(" ", "_"));
+            Console.ReadKey();
+        }
+    }
+}
